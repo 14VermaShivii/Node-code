9d7bd37c75a519a8a961edf811ba16f2cc78648a
shivani verma <vermashivi493@gmail.com>
2023-12-26 19:35:17 +0530

    here is node.js code

---
+++ App.js
@@ -0,0 +1,5 @@
+const {addition,multiply} =require("./Calculation")
+console.log("i started  learning node")
+console.log(addition(10,10))
+console.log(multiply(10,10))
+
---
+++ Calculation.js
@@ -0,0 +1,12 @@
+// function addition(a,b) {
+//     return a+b
+// }
+// function multiply(c,d){
+//     return c*d
+// }
+// module.exports={addition,multiply}
+// module.exports=addition
+//************************************************************ */
+
+exports.addition=(a,b)=>a+b
+exports.multiply=(c,d)=>c*d
